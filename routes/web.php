e9fd6f0d730b2347338cd7ee1f84fe098068e362
cristaldevil <cristaldevil@mail.ru>
2024-11-06 19:17:43 +0700

    Модуль Auth для Web

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+// Маршруты для регистрации, аутентификации и выхода
+use \App\Http\Controllers\Web\AuthController;
+Route::get('/register', [AuthController::class, 'showRegistrationForm']);
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::get('/login', [AuthController::class, 'showLoginForm']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:web');
